7956c02a08dc206090bb182c3b41c7ffdc10791e
bharatk101 <kulkarnibharat018@gmail.com>
2022-01-17 22:46:21 +0530

    Added DApp Token

---
+++ contracts/TokenFarm.sol
@@ -0,0 +1,103 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+
+contract TokenFarm is Ownable {
+    // stake tokens
+    // unstake token
+    // issue token
+    // addallowed ETH
+    //  get ETH Value
+
+    address[] public allowedTokens;
+    mapping(address => mapping(address => uint256)) public stakingBalance;
+    mapping(address => uint256) public uniqueTokensStaked;
+    mapping(address => address) public tokenPriceFeedMapping;
+    address[] public stakers;
+    IERC20 public dappToken;
+
+    constructor(address _dappTokenAddress) public {
+        dappToken = _dappTokenAddress;
+    }
+
+    function issueToken() public onlyOwner {
+        for (uint256 i = 0; i < stakers.length; i++) {
+            address receipent = stakers[i];
+            // send them token reward based on their total value locked
+            uint256 userTotalValue = getUserTotalValue(receipent);
+        }
+    }
+
+    function getUserTotalValue(address _user) public view returns(uint256) {
+        uint256 totalValue = 0;
+        require(uniqueTokensStaked[_user] > 0, "No Tokens staked!");
+
+        for (uint256 i = 0; i < allowedTokens.lenght; i++ ){
+            totalValue = totalValue + getUserSingleTokenValue(_user, allowedTokens[i]);
+        }
+
+    }
+
+
+    function getUserSingleTokenValue(address _user, address _token) public view returns(uint256){
+        if(uniqueTokensStaked[_user] <= 0){
+            return 0;
+        }
+        // price of token * token balance 
+        (uint256 price, uint256 decimals) =  getTokenValue(_token);
+        // return(stakingBalance[_token][_user])
+
+    }
+
+
+    function getTokenValue(address _token) public view returns(uint256, uint256) {
+        address priceFeedAddress = tokenPriceFeedMapping[_token];
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(priceFeedAddress);
+       (,int256 price,,,) =  priceFeed.latestRoundData();
+       uint256 decimals = uint256(priceFeed.decimals());
+       return (uint256(price), decimals);
+    }
+
+    function setPriceFeedContract(address _token, address _priceFeed) public onlyOwner {
+        tokenPriceFeedMapping[_token] = _priceFeed;
+    }
+
+
+    function stakeTokens(uint256 _amount, address _token) public {
+        require(_amount > 0, "Amount must be more than 0");
+        require(tokenIsAllowed(_token), "Token is currently not allowed");
+        // transferFrom
+        IERC20(_token).transferFrom(msg.sender, address(this), _amount);
+        updateUniqueTokensStakes(msg.sender, _token);
+        stakingBalance[_token][msg.sender] =
+            stakingBalance[_token][msg.sender] +
+            _amount;
+        if (uniqueTokensStaked[msg.sender] == 1) {
+            stakers.push(msg.sender);
+        }
+    }
+
+    function updateUniqueTokensStakes(address _user, address _token) internal {
+        if (stakingBalance[_token][_user] <= 0) {
+            uniqueTokensStaked[_user] = uniqueTokensStaked[_user] + 1;
+        }
+    }
+
+    function addAllowedTokens(address _token) public onlyOwner {
+        allowedTokens.push(_token);
+    }
+
+    function tokenIsAllowed(address _token) public returns (bool) {
+        for (uint256 i = 0; i < allowedTokens.length; i++) {
+            if (allowedTokens[i] == _token) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
