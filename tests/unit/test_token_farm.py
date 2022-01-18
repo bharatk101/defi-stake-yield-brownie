bc36e1e8510d474ebebfd815dc2367ec6ec7ddaf
bharatk101 <kulkarnibharat018@gmail.com>
2022-01-18 22:05:05 +0530

    Added tests for TokenFarm contract

---
+++ tests/unit/test_token_farm.py
@@ -0,0 +1,60 @@
+from lib2to3.pgen2 import token
+from brownie import network, exceptions
+from scripts.helpgul_scripts import INITIAL_VALUE, get_account, LOCAL_BLOCKCHAIN_ENVIRONMENTS, get_contract
+from scripts.deploy import deploy_token_farm_and_dapp_token
+import pytest
+
+
+def test_set_price_feed_contract():
+    # arrange
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    account = get_account()
+    non_owner = get_account(index=1)
+    token_farm, dapp_token = deploy_token_farm_and_dapp_token()
+    # act
+    price_feed_address = get_contract(
+        "eth_usd_price_feed")
+    token_farm.setPriceFeedContract(
+        dapp_token.address, price_feed_address, {"from": account})
+    # assert
+    assert token_farm.tokenPriceFeedMapping(
+        dapp_token.address) == price_feed_address
+
+    with pytest.raises(exceptions.VirtualMachineError):
+        token_farm.setPriceFeedContract(
+            dapp_token.address, price_feed_address, {"from": non_owner})
+
+
+def test_issue_tokens(amount_staked):
+    # arrange
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    account = get_account()
+    token_farm, dapp_token = test_stake_tokens(amount_staked)
+    starting_balance = dapp_token.balanceOf(account.address)
+    # acting
+    token_farm.issueToken({"from": account})
+    # arrange
+    assert(dapp_token.balanceOf(account.address)
+           == starting_balance + INITIAL_VALUE)
+
+
+def test_stake_tokens(amount_staked):
+    # arrange
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    account = get_account()
+    token_farm, dapp_token = deploy_token_farm_and_dapp_token()
+    # act
+    dapp_token.approve(token_farm.address, amount_staked, {"from": account})
+    token_farm.stakeTokens(
+        amount_staked, dapp_token.address, {"from": account})
+    # assert
+    assert (
+        token_farm.stakingBalance(
+            dapp_token.address, account.address) == amount_staked
+    )
+    assert(token_farm.uniqueTokensStaked(account.address) == 1)
+    assert(token_farm.stakers(0) == account.address)
+    return token_farm, dapp_token
